c63f92c64c6115c474e1b86dab6bd2009472513a
harshitgoel96 <harshitgoel96@yahoo.com>
2015-08-17 09:04:12 +0100

    added arc demo subdir

---
+++ arc-demo/js/app.js
@@ -0,0 +1,1 @@
+var app=angular.module('arcDemo',['Controllers','angular-progress-arc']);
---
+++ arc-demo/js/controllers.js
@@ -0,0 +1,7 @@
+var app=angular.module('Controllers',[]);
+
+app.controller('arcCtrl',function($scope){
+$scope.arcPerc=75;
+$scope.arcText="some text";
+$scope.isClockwise="false";
+});
